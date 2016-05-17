3f9d554155a124a6259b8373564ecc5f19c4d415
Raphael Coeffic <rco@iptel.org>
2016-05-17 18:50:06 +0200

    reworked to support Arduinos (Pro mini) + status LED

--- SportECU.ino
+++ SportECU.ino
@@ -3,7 +3,11 @@
 #include "config.h"
 #include "FrskySP.h"
 
-#define ECU_HELLO       "+++"
+// same on arduinos & teensy LC & 3.x
+#define LED_PIN 13
+
+#define ECU_INIT_TIMEOUT 2000 /* 2 seconds */
+#define ECU_PLUS          '+'
 #define ECU_EOL          0x0D
 
 #define ECU_FRAME_START  0x7E
@@ -22,24 +26,6 @@
   // Hardware serial port is used on Arduinos
   #define ecuSerial Serial
 #endif
-
-void setup()
-{
-  ecuSerial.begin(ECU_BIT_RATE);
-  FrskySP.begin(SPORT_SENSOR_ID,6);
-
-#ifdef USE_TEENSY
-  Serial.begin(DBG_SERIAL_BIT_RATE);
-  Serial.println("*** Serial Hornet ***");
-#endif
-
-  while(!ecuSerial.find((char*)ECU_HELLO));
-  ecuSerial.print("OK\r");
-
-#ifdef USE_TEENSY
-  Serial.println("+++ -> OK");
-#endif
-}
 
 #define T1_FIRST_ID               0x0400
 #define T2_FIRST_ID               0x0410
@@ -84,68 +70,157 @@ void processEcuFrame(uint8_t* frame)
   FrskySP.setSensorData(5,T2_FIRST_ID,status);
 }
 
-#define BUFFER_SIZE 64
+unsigned long start_ts = 0;
+const char OK_str[] = "OK\r";
+
+void setup()
+{
+  ecuSerial.begin(ECU_BIT_RATE);
+  FrskySP.begin(SPORT_SENSOR_ID,6);
+
+#ifdef USE_TEENSY
+  Serial.begin(DBG_SERIAL_BIT_RATE);
+  Serial.println("*** Serial Hornet ***");
+#endif
+
+#ifdef USE_TEENSY
+  Serial.println("+++ -> OK");
+#endif
+
+  pinMode(LED_PIN, OUTPUT);
+  start_ts = millis();
+}
 
 void loop()
 {
+  enum {
+    INIT_PLUS_1=0,
+    INIT_PLUS_2,
+    INIT_PLUS_3,
+    INIT_WAIT_4_FRAME,
+    INIT_RUN
+  };
+  static uint8_t init_mode = INIT_PLUS_1;
+
+  static uint8_t led_blink_rate = 1; // start with slow blinking
+  static unsigned long led_ts = 0;
+  
   static unsigned char buffer[ECU_FRAME_LENGTH];
   static uint8_t frame_length = 0;
 
-  static bool init_mode = true;
   static bool byte_stuffing = false;
   static uint8_t chk_sum = 0;
 
+  // Send OK to kickstart the ECU
+  // in case the boot loader was
+  // still running as the hello
+  // message was sent.
+  //
+  if( (init_mode == INIT_PLUS_1)
+      && (millis() - start_ts > ECU_INIT_TIMEOUT)) {
+
+    ecuSerial.print(OK_str);
+    start_ts = millis();
+  }
+  
   FrskySP.poll();
 
   while(ecuSerial.available()){
 
     uint8_t data = ecuSerial.read();
 
-    if(init_mode) {
+    switch(init_mode) {
+
+    case INIT_PLUS_1:
+    case INIT_PLUS_2:
+    case INIT_PLUS_3:
+
+      if(data == ECU_PLUS) {
+        if(init_mode++ == INIT_PLUS_3) {
+          ecuSerial.print(OK_str);
+          led_blink_rate = 3; // blink fast
+          
+#ifdef USE_TEENSY
+          Serial.println("OK");
+#endif
+        }
+
+        continue;
+      }
+      // fall through to the next case
+      // in case OK has been already
+      // sent without seeing '+++'
+
+    case INIT_WAIT_4_FRAME:
+
       if(data == ECU_EOL) {
-        ecuSerial.print("OK\r");
+        ecuSerial.print(OK_str);
+
 #ifdef USE_TEENSY
         Serial.println("OK");
 #endif
+        continue;
       }
-      else if(data == ECU_FRAME_START) {
-        init_mode = false;
+
+      if(data != ECU_FRAME_START)
+        continue;
+
+      init_mode = INIT_RUN;
+      led_blink_rate = 0; // stay on
+
+#ifdef USE_TEENSY
+      Serial.println("ECU Wireless telemetry initialized");
+#endif
+      // continue with 'case INIT_RUN:'
+
+    case INIT_RUN:
+
+      if(data == ECU_FRAME_START) {
         frame_length = 0;
         chk_sum = 0;
+        continue;
       }
-      continue;
-    }
-    else if(data == ECU_FRAME_START) {
-      frame_length = 0;
-      chk_sum = 0;
-      continue;
-    }
 
-    if(data == ECU_BYTE_STUFF) {
-      byte_stuffing = true;
-      continue;
-    }
+      if(data == ECU_BYTE_STUFF) {
+        byte_stuffing = true;
+        continue;
+      }
 
-    if(byte_stuffing) {
-      data ^= ECU_STUFF_MASK;
-      byte_stuffing = false;
-    }
+      if(byte_stuffing) {
+        data ^= ECU_STUFF_MASK;
+        byte_stuffing = false;
+      }
     
-    buffer[frame_length++] = data;
-    chk_sum += data;
+      buffer[frame_length++] = data;
+      chk_sum += data;
 
-    if(frame_length == ECU_FRAME_LENGTH) {
-      if(chk_sum == ECU_CHKSUM_GOOD) {
-        processEcuFrame(buffer);
-      }
+      if(frame_length == ECU_FRAME_LENGTH) {
+        if(chk_sum == ECU_CHKSUM_GOOD) {
+          processEcuFrame(buffer);
+        }
 #ifdef USE_TEENSY
-      else {
-        Serial.print("## checksum error (0x");
-        Serial.print(chk_sum,HEX);
-        Serial.println(") ##");
-      }
+        else {
+          Serial.print("## checksum error (0x");
+          Serial.print(chk_sum,HEX);
+          Serial.println(") ##");
+        }
 #endif
-      frame_length = 0;
+        frame_length = 0;
+      }
+      break;
+    }
+  }
+
+  if(!led_blink_rate) {
+    digitalWrite(LED_PIN,HIGH);
+  }
+  else {
+    unsigned long now_ts = millis();
+    
+    if(now_ts - led_ts > (2000 >> led_blink_rate)) {
+      // toggle LED pin
+      digitalWrite(LED_PIN,!digitalRead(LED_PIN));
+      led_ts = now_ts;
     }
   }
 }
